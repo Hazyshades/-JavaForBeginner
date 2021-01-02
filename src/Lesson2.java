38db158d586aefb76ad74c3f78da714162bf030d
LeoKolbado <gold60822@gmail.com>
2021-01-02 10:24:59 +0300

    commit lesson 2 (variables)

---
+++ src/Lesson2.java
@@ -0,0 +1,20 @@
+public class Lesson2 {
+    public static void main(String[] args) {
+
+        //целочисленные типы данных
+        long myLong = 2147121212; //64-bit
+        int x = 2147483647; // примитивный тип, 32-bit, 2147483647; -2147483648, можно найти через класс Integer.MAX_VALUE
+        short myShort = 32767; //16-bit,
+        byte myByte = -128; //8-bit, -128;127
+
+        //вещественные типы данных
+        double myDouble = 123.55; //64-bit
+        float myFloat = 123.44f; //32-bit, для floal добавляем "f"
+
+        char c = 'a'; //один символ-буква
+
+        //логический тип данных
+        boolean b = true;
+        
+    }
+}
