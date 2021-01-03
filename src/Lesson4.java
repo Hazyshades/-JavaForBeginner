c6382a8ced131a9772ff0351d6d171253d1a0119
hazyshades <gold60822@gmail.com>
2021-01-03 13:10:46 +0300

    commit lesson 4

---
+++ src/Lesson4.java
@@ -0,0 +1,15 @@
+public class Lesson4 {
+    public static void main(String[] args) {
+      boolean a = 6==6; //== операция сравнивания
+
+
+        int value = 0;
+        boolean t = value>5;
+        System.out.println(t);
+        //метод выполняется, пока value меньше 5
+        while(value<5){
+            System.out.println("Всё ок");
+        value=value+1;
+        }
+    }
+}
