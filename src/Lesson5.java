9d6277fcfb18be01216ebc82eac8edbdef12673a
hazyshades <gold60822@gmail.com>
2021-01-07 12:35:24 +0300

    commit lesson 5

---
+++ src/Lesson5.java
@@ -0,0 +1,11 @@
+public class Lesson5 {
+    public static void main(String[] args) {
+
+       int i;
+        for(i=10; i>=5; i--){                //i=i+1 это i++;      i=i-1 это i--
+            System.out.println("Hello + " + i);
+
+            }
+        }
+    }
+
