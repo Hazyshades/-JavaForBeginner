67b1de5c559d73498672ced0c67dce2ae3bd1482
hazyshades <gold60822@gmail.com>
2021-01-07 12:46:31 +0300

    commit lesson_6

---
+++ src/Lesson6.java
@@ -0,0 +1,14 @@
+public class Lesson6 {
+    public static void main(String[] args) {
+        int x=15;
+        if (x<10) {
+            System.out.println("Вывожу первое условие");
+
+        } else if (x>15){
+            System.out.println("Вывожу второе условие");
+        }
+        else{
+            System.out.println("Иначе вывожу это");
+        }
+    }
+}
